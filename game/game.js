e01595e92fcacea8966063c12a92929975755034
kamenkamen <kamen.lilov@gmail.com>
2019-03-08 12:20:57 +0200

    Add functions for drop elements from sky and shooting

--- game/game.js
+++ game/game.js
@@ -7,65 +7,40 @@ document.onmousemove = function (e) {
 };
 
 let currentWeapon = 'pistol';
+let kills = 0;
 
 function game() {
     // DOM elements
     let bodyElement = document.getElementsByTagName("BODY")[0];
-    let startElement = document.querySelector('.start');
     let areaElement = document.querySelector('.area');
     let bullets = document.querySelector('.bullets p');
     let bombs = document.querySelector('.bombs p');
 
     // load functions
-    unloadScrollBars();
+    hideScrollBars();
     setInterval(changeGunPosition, 100);
-    // setInterval(backgroundSound, 10 * 1000);
-
-    // welcome screen event on click enter
-    let startWindow = function (event) {
-        if ('Enter' === event.code) {
-            startElement.classList.add('hide');
-            playAudio('./sounds/start-sound.mp3');
-            document.removeEventListener('keyup', startWindow);
-        }
-    };
+    setInterval(backgroundSound, 7 * 1000);
 
     // welcome screen
-    document.addEventListener('keyup', startWindow);
+    document.addEventListener('keyup', welcomeScreenEvent);
 
     // change weapon
     document.addEventListener('keyup', function (event) {
-        let gunElement = document.querySelector('.gun');
-
         if ('Digit1' === event.code) {
-            gunElement.innerHTML = ''; // clear previous weapon
-            let rifleElement = document.createElement('div'); // create div for pistol
-            rifleElement.classList.add('pistol'); // add class .pistol
-            currentWeapon = 'pistol';
-            gunElement.appendChild(rifleElement);
+            setNewWeapon('pistol');
         } else if ('Digit2' === event.code) {
-            gunElement.innerHTML = ''; // clear previous weapon
-            let rifleElement = document.createElement('div'); // create fiv for rifle
-            rifleElement.classList.add('rifle'); // add class .rifle
-            currentWeapon = 'rifle';
-            gunElement.appendChild(rifleElement);
+            setNewWeapon('rifle');
         } else if ('Digit3' === event.code) {
-            gunElement.innerHTML = ''; // clear previous weapon
-            let rifleElement = document.createElement('div'); // create fiv for banana
-            rifleElement.classList.add('banana'); // add class .banana
-            currentWeapon = 'banana';
-            gunElement.appendChild(rifleElement);
+            setNewWeapon('banana');
         } else if ('Digit4' === event.code) {
-            bodyElement.style.opacity = 0;
-            removeWhiteScreenAfterNukeBomb();
-            console.log('DROP NUKE');
+            dropNukeBomb();
         }
     });
 
     // on shoot with left mouse
     document.addEventListener('click', function () {
         if ('pistol' === currentWeapon) {
-            bullets.textContent -= 1; // remove bullets on shot with pistol
+            bullets.textContent -= 1; // remove one bullet on shot with pistol
             playAudio('./sounds/gun-sound.wav');
         } else if ('rifle' === currentWeapon) {
             bullets.textContent -= 5; // remove 5 bullets on shot with AK-47
@@ -74,7 +49,23 @@ function game() {
             bullets.textContent = parseInt(bullets.textContent) + 50; // set GOD_mode with banana
             playAudio('./sounds/banana-sound.wav');
         }
+
+        onShotDropElement();
     });
+}
+
+function welcomeScreenEvent(event) {
+    let startElement = document.querySelector('.start');
+    let gameSpeed = 700;
+
+    if ('Enter' === event.code) {
+        startElement.classList.add('hide'); // hide welcome menu
+        playAudio('./sounds/start-sound.mp3'); // play start sound
+        setInterval(generateElementsForDrops, gameSpeed); // start generate elements for drops
+        document.removeEventListener('keyup', welcomeScreenEvent); // remove event listener for Enter button
+        let dinko = document.querySelector('.dinko');
+        dinko.style.display = 'none'; // hide Dinko
+    }
 }
 
 function playAudio(soundPath) {
@@ -82,7 +73,7 @@ function playAudio(soundPath) {
     audio.play();
 }
 
-function unloadScrollBars() {
+function hideScrollBars() {
     document.documentElement.style.overflow = 'hidden';
     document.body.scroll = "no";
 }
@@ -97,7 +88,90 @@ function backgroundSound() {
     playAudio('./sounds/background-sound.wav');
 }
 
-function removeWhiteScreenAfterNukeBomb() {
+function dropNukeBomb() {
     let bodyElement = document.getElementsByTagName("BODY")[0];
-    bodyElement.style.opacity = 1;
+    playAudio('./sounds/bomb-sound.wav');
+
+    setTimeout(function () {
+        bodyElement.style.opacity = 0;
+    }, 3100);
+
+    setTimeout(function () {
+        let killsElement = document.querySelector('.kills p');
+        let countParachutesOnScreen = document.querySelectorAll('.drop-element').length; // all parachutes in div dropElements
+
+        killsElement.textContent = parseInt(killsElement.textContent) + countParachutesOnScreen; // add killed parachutes into kills;
+
+        let dropElementsDiv = document.querySelector('.drop-elements');
+        dropElementsDiv.textContent = ''; // remove all elements on screen
+
+        bodyElement.style.opacity = 1;
+    }, 7000);
+
+    let bombs = document.querySelector('.bombs p');
+    bombs.textContent -= 1;
+}
+
+function setNewWeapon(weaponName) {
+    let gunElement = document.querySelector('.gun');
+
+    gunElement.innerHTML = ''; // clear previous weapon
+    let rifleElement = document.createElement('div'); // create div for pistol
+    rifleElement.classList.add(weaponName); // add class .pistol
+    currentWeapon = weaponName;
+    gunElement.appendChild(rifleElement);
+}
+
+function generateElementsForDrops() {
+    let marginLeft = 10;
+    let marginRight = 70;
+    let randomWidthForNewElement = Math.floor(Math.random() * (screen.width - marginRight)) + marginLeft;
+
+    let parachute = document.createElement('div');
+    parachute.classList.add('parachute');
+    let tomcat = document.createElement('div');
+    tomcat.classList.add('tomcat');
+
+    let dropElement = document.createElement('div');
+    dropElement.classList.add('drop-element');
+    dropElement.style.left = randomWidthForNewElement + 'px';
+    dropElement.appendChild(parachute);
+    dropElement.appendChild(tomcat);
+
+    let dropElements = document.querySelector('.drop-elements');
+    dropElements.appendChild(dropElement);
+
+    moveDownElements();
+}
+
+function moveDownElements() {
+    let dropElements = document.querySelectorAll('.drop-element');
+
+    for (const element of dropElements) {
+        let position = element.style.top;
+        let id = setInterval(frame, 10);
+
+        function frame() {
+            if (position === screen.height - 200) {
+                element.remove(); // remove element if drop down
+                clearInterval(id);
+            } else {
+                position++;
+                element.style.top = position + 'px';
+            }
+        }
+    }
+}
+
+function onShotDropElement() {
+    let dropElements = document.querySelectorAll('.drop-element');
+    let killsElement = document.querySelector('.kills p');
+
+    for (const element of dropElements) {
+        element.addEventListener('click', function (event) {
+            element.remove();
+            kills++;
+            killsElement.textContent = kills;
+        });
+    }
 }
